bc7ea273ed98b8f15c4164344333d95afe113632
Cody Dupuis <ctdupuis7@gmail.com>
2019-10-03 19:43:26 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -19,4 +19,8 @@ class Doctor
   def appointments
     Appointment.all.select{|appointment| appointment.doctor == self}
   end
+  
+  def patients
+    appointments.collect{|apt| apt.patient}
+  end
 end
