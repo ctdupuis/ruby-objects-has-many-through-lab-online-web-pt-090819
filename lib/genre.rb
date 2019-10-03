817211d99e6c6514dda06244c4deab36e01eb981
Cody Dupuis <ctdupuis7@gmail.com>
2019-10-03 19:18:07 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -12,15 +12,5 @@ class Genre
     @@all
   end
   
-  def new_song(name)
-    Song.new(name, self, genre)
-  end
   
-  def songs 
-    Song.all.select{|song| song.artist == self}
-  end
-  
-  def genres 
-    songs.collect{|song| song.genre}
-  end
 end
